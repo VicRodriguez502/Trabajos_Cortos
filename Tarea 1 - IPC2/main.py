2850c57e888b9c8afd0657edc1563fea4f8ba121
VicRodriguez502 <3015506460101@ingenieria.usac.edu.gt>
2023-02-03 20:26:14 -0600

    Terminando el prgrama que permite ordenar los datos por edades de una lista doblemente enlazada

--- Tarea 1 - IPC2/main.py
+++ Tarea 1 - IPC2/main.py
@@ -1,5 +1,3 @@
-#Tarea #1 Creada por Victor Rodriguez Carnet 201900018
-
 #Creación de la clase Nodo
 class nodoPersona():
     #Constructor del nodo para la lista Doble
@@ -22,7 +20,7 @@ class ListaDoble:
     def vacia(self):
         return self.primero == self.ultimo == None
 
-    #Método para agregar celdas a la lista celdas
+    #Método para agregar Personas a la lista doble
     def agregarNodoP(self, id, nombre, edad):
         nuevaPersona = nodoPersona(id, nombre, edad)
         if self.vacia():
@@ -37,19 +35,64 @@ class ListaDoble:
             self.ultimo = nuevaPersona
         self.tamanio = self.tamanio + 1
 
+    #Mostrar Personas mediante el ordenamiento por edad
     def mostrarPersona(self):
         actual = self.primero
-        while(actual != None):
-            print('ID: ', str(actual.id), 'Nombre: ', str(actual.nombre), 'Edad: ', str(actual.edad))
+        while (actual != None):
+            print('ID: ', str(actual.id), 'Nombre: ', str(
+                actual.nombre), 'Edad: ', str(actual.edad))
             print('--------------------------------------------------')
             actual = actual.siguiente
+    
+    #Creación del método ordenamiento para ordenar los datos por medio de la edad
+    def ordenamientoListaDouble(self):
+        # Verifica si la lista está vacía o tiene un solo elemento
+        if self.vacia() or self.primero == self.ultimo:
+            return
+        # En caso contrario, comienza el ordenamiento por inserción
+        else:
+            # Crea una variable temporal `temp` que apunta al segundo elemento de la lista
+            temp = self.primero.siguiente
+            
+            # Itera a través de cada elemento de la lista después del primer elemento
+            while temp:
+                # Almacena los valores actuales de `temp` en variables temporales
+                val = temp.edad
+                val_id = temp.id
+                val_nombre = temp.nombre
+                
+                # Crea una variable temporal `temp_antes` que apunta al elemento anterior de `temp`
+                temp_antes = temp.antes
+                
+                # Mientras `temp_antes` no sea `None` y su valor de edad sea mayor que el valor actual de `temp`,
+                # mueve el valor de `temp_antes` a su siguiente elemento y actualiza las variables temporales
+                while temp_antes and temp_antes.edad > val:
+                    temp_antes.siguiente.edad = temp_antes.edad
+                    temp_antes.siguiente.id = temp_antes.id
+                    temp_antes.siguiente.nombre = temp_antes.nombre
+                    temp = temp_antes
+                    temp_antes = temp_antes.antes
+                # Asigna los valores temporales al elemento actual `temp`
+                temp.edad = val
+                temp.id = val_id
+                temp.nombre = val_nombre
+                
+                # Actualiza `temp` para apuntar al siguiente elemento
+                temp = temp.siguiente
 
 lista = ListaDoble()
 
 #Creación del main
 if __name__ == '__main__':
-    print('Hola mundo')
-    lista.agregarNodoP(1,'Victor', 25)
-    lista.agregarNodoP(2,'Marcos',22)
-    lista.agregarNodoP(3,'Aaron',23)
+    print('Datos Agregados manualmente')
+    lista.agregarNodoP(1, 'Victor', 25)
+    lista.agregarNodoP(2, 'Marcos', 22)
+    lista.agregarNodoP(3, 'Aaron', 23)
+    lista.agregarNodoP(4, 'Salas', 5)
+    lista.agregarNodoP(5, 'Rey', 40)
+    lista.agregarNodoP(6, 'July', 11)
+    lista.mostrarPersona()
+
+    print("Después de esto el ordenamiento se aplica al obejto de la clase ListaDouble")
+    lista.ordenamientoListaDouble()
     lista.mostrarPersona()
